96fbcdc844dfb202cb592f001363eb2818e5bf8a
Dillon Friel <dillonfriel@gmail.com>
2018-08-28 21:24:41 +0100

    basic react components working

---
+++ src/app/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export class Header extends React.Component {
+	render () {
+		return (
+			<header>
+        <nav class="navbar navbar-expand-lg navbar-transparent navbar-dark bg-dark py-3" id="navbar_main">
+            Ok
+        </nav>
+    </header>
+		);
+	}
+}
--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,33 @@
+import React from "react";
+import { render } from "react-dom";
+
+import { Header } from ".\\components\\Header";
+import { Home } from ".\\components\\Home";
+
+class App extends React.Component {
+	render() {
+
+		/* Remember: This is not returning HTML code 
+		In the background this is like JavScript writing HTML code piece by piece
+		E.g. create div, use document.getElement..innerHTML = "some text" etc.		
+		*/
+		return (
+			<div className="container">
+				<div className="row">
+					<div className="col-xs-10 col-xs-offset-1">
+						<Header/>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-xs-10 col-xs-offset-1">
+						<Home/>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+render(<App/>, window.document.getElementById("app"));
+
 console.log("It works!!!");
